52bc21fe66a00c0ad8f45b183c182da80778af62
Gcsainiuharvest <gopi.chand@uharvest.in>
2025-08-04 16:06:23 +0530

    fixed api url

--- src/app/api/constants.ts
+++ src/app/api/constants.ts
@@ -1,2 +1,2 @@
-//export const baseApiUrl = "https://uharvest-non-prod-evfrc2d5f6hpa5dn.centralindia-01.azurewebsites.net/";
- export const baseApiUrl = "http://localhost:5001";
+export const baseApiUrl = "https://uharvest-non-prod-evfrc2d5f6hpa5dn.centralindia-01.azurewebsites.net/";
+//  export const baseApiUrl = "http://localhost:5001";
